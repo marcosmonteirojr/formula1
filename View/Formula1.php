d78388f54168608bbf61ff59ca162ac465826081
marcosmonteirojr <marcos.monteirojr@gmail.com>
2024-05-14 12:13:28 -0300

    Alteração_pagina_inicial

--- View/Formula1.php
+++ View/Formula1.php
@@ -1,24 +1,25 @@
 <div class="container px-4 py-5" id="custom-cards">
     <div class="row row-cols-1 row-cols-lg-4 align-items-stretch ">
-        <div class="card text-white bg-dark" style="width: 18rem;">
-        <div class="card-header">Ferrari</div>    
-        <img class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" src="../View/img/ferrari.jpg" alt="Card image cap">
+
+        <div class="card text-white bg-dark border-0" style="width: 18rem;">
+            <div class="card-header border-0 text-bg-dark">Ferrari</div>
+            <img class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" src="../View/img/ferrari.jpg" alt="Card image cap">
             <div class="card-body">
-                <p class="card-text">Ferrari blablabla</p>
+
             </div>
         </div>
-        <div class="card text-white bg-dark" style="width: 18rem;">
-        <div class="card-header">Mercedes Bens</div>
+        <div class="card text-white bg-dark border-0" style="width: 18rem;">
+            <div class="card-header border-0 text-bg-dark">Mercedes Bens</div>
             <img class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" src="../View/img/meca.jpg" alt="Card image cap">
             <div class="card-body">
-                <p class="card-text">Mercedes Bens blablabla</p>
+
             </div>
         </div>
-        <div class="card text-white bg-dark" style="width: 18rem;">
-        <div class="card-header">Red Bull</div>    
-        <img class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" src="../View/img/rb.jpg" alt="Card image cap">
+        <div class="card text-white bg-dark border-0 " style="width: 18rem;">
+            <div class="card-header border-0 text-bg-dark">Red Bull</div>
+            <img class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" src="../View/img/rb.jpg" alt="Card image cap">
             <div class="card-body">
-                <p class="card-text">Red Bull Racing blablabla</p>
+
             </div>
         </div>
     </div>
--- View/layout/topo.php
+++ View/layout/topo.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br"  data-bs-theme="dark">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 </head>
-
 <body>
-
-  <header class="p-3 bg-dark text-white">
+  <header class="p-3 bg-secondary text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
@@ -18,17 +15,11 @@
             <use xlink:href="#bootstrap"></use>
           </svg>
         </a>
-
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="/" class="nav-link px-2 text-secondary">Home</a></li>
           <li><a href="/piloto" class="nav-link px-2 text-white">Pilotos</a></li>
           <li><a href="/piloto/form" class="nav-link px-2 text-white">Cadastrar Pilotos</a></li>
         </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
-
         <div class="text-end">
           <button type="button" class="btn btn-outline-light me-2">Login</button>
           <button type="button" class="btn btn-warning">Sign-up</button>
@@ -43,9 +34,6 @@
 
   <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
     </ul>
