439736f7439ef91009c194ac6f87dbfa461667a2
marcosmonteirojr <marcos.monteirojr@gmail.com>
2024-05-14 13:06:55 -0300

    pequenas_alterações_topo_e_controller_geral

--- Controller/ControllerGeral.php
+++ Controller/ControllerGeral.php
@@ -2,7 +2,7 @@
 class ControllerGeral{
     public static function index(){
         
-        
+        $title = "Home";
         $page = 'View/Formula1.php';
         include 'View/layout/topo.php';
         //include 'View/modulos/Piloto/ListaPiloto.php'; // Include da View, propriedade $rows da Model pode ser acessada na View
--- View/layout/topo.php
+++ View/layout/topo.php
@@ -5,10 +5,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title><?php echo $title; ?></title>
 </head>
 <body>
-  <header class="p-3 bg-secondary text-white">
+  
     <div class="container">
+    <header class=" p-2 bg-secondary text-white rounded-1">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
@@ -16,7 +18,7 @@
           </svg>
         </a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="/" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="/" class="nav-link px-2 text-white ">Home</a></li>
           <li><a href="/piloto" class="nav-link px-2 text-white">Pilotos</a></li>
           <li><a href="/piloto/form" class="nav-link px-2 text-white">Cadastrar Pilotos</a></li>
         </ul>
